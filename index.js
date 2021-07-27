e769ba16c610e478051a0010c64ea7fa419c3df3
Emily N Mayeski <enmayeski@gmail.com>
2021-07-27 16:02:08 -0400

    closes #2 - Employee Parent Class takes in information from CLI and creates an object

--- index.js
+++ index.js
@@ -1,26 +1,46 @@
+// const Employee = require("./lib/Employee.js");
 const Engineer = require("./lib/Engineer.js");
 const inquirer = require("inquirer");
-const generatePage = require('./src/page-template');
-const { writeFile, copyFile } = require('./utils/generate-site');
+// const generatePage = require('./src/page-template');
+// const { writeFile, copyFile } = require('./utils/generate-site');
 
 
-const team = []
+const team = [];
 
 function promptUser(){
+    console.log(`
+=================
+Add a New Employee
+=================
+        `);    
+
     inquirer
     .prompt([
         {
             type: "input",
             name: "name",
-            message: "What is your name?"
+            message: "What is the employee's name?"
+
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is the employee's id? (Numbers Only)"
+
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is the employee's email address?"
 
         },
         {
             type: "list",
             name: "role",
-            message: "What is your job title?",
+            message: "What is the employee's title?",
             choices: ["Manager", "Engineer", "Intern"]
         }
+
     ]).then(function(answers) {
         if(answers.role === "Engineer"){
             return engineerQuestions(answers);
@@ -33,10 +53,10 @@ function engineerQuestions(employeeAnswers){
         {
             type: 'input',
             name: "username",
-            message: "What is your Github Username?"
+            message: "What is the employee's Github Username?"
         }
     ]).then(function(engineerAnswer){
-        let newEngineer = new Engineer(employeeAnswers.name, engineerAnswer.username )
+        let newEngineer = new Engineer(employeeAnswers.name, employeeAnswers.id, employeeAnswers.email, engineerAnswer.username)
         // console.log("ADDED AN ENGINEER", newEngineer)
         team.push(newEngineer)
         console.log("Your team so far:");
@@ -46,7 +66,6 @@ function engineerQuestions(employeeAnswers){
 }
 
 function addOrFinish(team) {
-
     inquirer
     .prompt([
         {
@@ -62,6 +81,7 @@ function addOrFinish(team) {
             return promptUser();
         } else {
             // return generatePage(team);
+            console.log("Your team:");
             console.log(team);
         }
     }
